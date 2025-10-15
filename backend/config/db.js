961ca685de1fcab6dcce3080c3a4dd1257fb6488
Rajasekhar Dronamraju <153741821+Rajasekhar318@users.noreply.github.com>
2025-10-15 13:14:36 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://jayaswaroopdandamudi:cpF4wGBpzJ7o2yKC@cluster0.8esjo.mongodb.net/masked-app').then(()=>console.log("DB connected"));
+    await mongoose.connect('<mongodb-url>').then(()=>console.log("DB connected"));
 }
